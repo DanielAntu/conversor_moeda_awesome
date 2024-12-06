bed03190da62e9e7fa9a24359880540247425d64
Daniel Antunes <antunesdaniel083@gmail.com>
2024-12-06 15:44:14 -0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,89 @@
+const currency1 = document.querySelector("#currency1");
+const currency2 = document.querySelector("#currency2");
+const selects = document.querySelectorAll("select");
+const ValueElement = document.querySelector("#value");
+const btn = document.querySelector(".btn");
+const resultContainer = document.querySelector(".result");
+const valueResult = document.querySelector(".value");
+
+let currencySelect1 = "";
+let currencySelect2 = "";
+
+const url = "https://economia.awesomeapi.com.br/";
+
+const getCurrencys = async () => {
+    const response = await fetch(`${url}available/uniq`);
+
+    const data = await response.json();
+    return data;
+};
+
+const createOptions = (currencys, el) => {
+    currencys.forEach((currency) => {
+        const option = document.createElement("option");
+        option.value = currency[0];
+        option.innerText = `${currency[0]} (${currency[1]})`;
+        el.appendChild(option);
+    });
+};
+
+const initialize = async () => {
+    data = await getCurrencys();
+    const currencys = [];
+
+    for (var i in data) {
+        currencys.push([i, data[i]]);
+    }
+
+    createOptions(currencys, currency1);
+    createOptions(currencys, currency2);
+};
+
+const getQuote = async (currency1, currency2) => {
+    currencyPair = `${currency1}-${currency2}`;
+    key = `${currency1}${currency2}`;
+    const response = await fetch(`${url}json/last/${currencyPair}`);
+    const data = await response.json();
+    if (data.status === 404) return;
+    return data[key].bid;
+};
+
+selects.forEach((select) => {
+    select.addEventListener("change", (e) => {
+        if (select.name == "currency1") {
+            currencySelect1 = e.target.value;
+        }
+
+        if (select.name == "currency2") {
+            currencySelect2 = e.target.value;
+        }
+    });
+});
+
+btn.addEventListener("click", async () => {
+    valueConvert = ValueElement.value;
+
+    if ((!currencySelect1 || !currencySelect2, !valueConvert)) {
+        resultContainer.classList.add("hide");
+        return;
+    }
+
+    if (currencySelect1 === currencySelect2) {
+        resultContainer.classList.add("hide");
+        return;
+    }
+
+    quote = await getQuote(currencySelect1, currencySelect2);
+
+    if (!quote) {
+        resultContainer.classList.add("hide");
+        return;
+    }
+
+    const convert = valueConvert * quote;
+
+    resultContainer.classList.remove("hide");
+    valueResult.innerText = `${convert.toFixed(2)}`;
+});
+
+initialize();
